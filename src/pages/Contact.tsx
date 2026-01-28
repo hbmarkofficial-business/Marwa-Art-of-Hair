7c887fbb16e85ddc0a77eefd8c8b257aa1276a64
hbmarkofficial-business <hbmark.official@gmail.com>
2026-01-28 16:33:38 +0100

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -131,7 +131,7 @@ const Contact = () => {
 
                   <div className="aspect-video rounded-sm overflow-hidden border border-border">
                     <iframe
-                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2593.0466369434594!2d11.117062376893147!3d49.464733457845!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479f57dec87eba7f%3A0xac674ba124944238!2sLord%20Barbershop%20Coiffeur!5e0!3m2!1sde!2sde!4v1769174581720!5m2!1sde!2sde"
+                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2593.852568306081!2d11.061355176892453!3d49.4495034589262!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479f575fbdf64965%3A0x1967f663ea995e5a!2sMarwa%20Art%20of%20Hair!5e0!3m2!1sde!2sde!4v1769614728252!5m2!1sde!2sde"
                       width="100%"
                       height="100%"
                       style={{ border: 0, filter: 'grayscale(100%) contrast(1.2)' }}
@@ -154,6 +154,7 @@ const Contact = () => {
 };
 
 export default Contact;
+
 
 
 
